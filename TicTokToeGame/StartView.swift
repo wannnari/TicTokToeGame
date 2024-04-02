e35b782434b2eeb1c0e9a1cc5c1038aeb45d217b
kazu <Walk.aaaiaua@gmail.com>
2024-04-02 22:48:37 +0900

    マルバツゲームの枠を作成

---
+++ TicTokToeGame/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  TicTokToeGame
+//
+//  Created by 中神和也 on 2024/04/02.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
