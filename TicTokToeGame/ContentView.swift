97f751525d60e11ed0caf354409889c65f43dd86
kazu <Walk.aaaiaua@gmail.com>
2024-04-02 22:19:10 +0900

    Initial Commit

---
+++ TicTokToeGame/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TicTokToeGame
+//
+//  Created by 中神和也 on 2024/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+            Text("tic-tok-towGame!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTokToeGame/TicTokToeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTokToeGameApp.swift
+//  TicTokToeGame
+//
+//  Created by 中神和也 on 2024/04/02.
+//
+
+import SwiftUI
+
+@main
+struct TicTokToeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
